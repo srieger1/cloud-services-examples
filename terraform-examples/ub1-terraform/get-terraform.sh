407612b109387f84e9f894b4edda89e1aa935161
Sebastian Rieger <sebastian.rieger@informatik.hs-fulda.de>
2024-11-05 11:44:47 +0100

    updated initial terraform example for winter term 2024 using new Charmed OpenStack env

--- terraform-examples/ub1-terraform/get-terraform.sh
+++ terraform-examples/ub1-terraform/get-terraform.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-wget https://releases.hashicorp.com/terraform/1.1.3/terraform_1.1.3_linux_amd64.zip -O terraform_1.1.3_linux_amd64.zip
-unzip -o terraform_1.1.3_linux_amd64.zip
+
+# e.g.,: use the recent stable version, see https://developer.hashicorp.com/terraform/install
+# you can also use OpenTofu as an alternative (https://opentofu.org/)
+
+#wget https://releases.hashicorp.com/terraform/1.1.3/terraform_1.1.3_linux_amd64.zip -O terraform_1.1.3_linux_amd64.zip
+#unzip -o terraform_1.1.3_linux_amd64.zip
