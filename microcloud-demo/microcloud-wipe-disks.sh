0890cfcd17ef6b4c66a8ec7f03db2a08d0ab6c91
Sebastian Rieger <sebastian.rieger@informatik.hs-fulda.de>
2024-11-06 22:56:40 +0100

    microcloud-wipe-disks.sh aktualisieren

--- microcloud-demo/microcloud-wipe-disks.sh
+++ microcloud-demo/microcloud-wipe-disks.sh
@@ -2,7 +2,7 @@
 # does not clean everything - strangely
 #sudo wipefs -a /dev/sda
 #sudo wipefs -a /dev/sdb
-sudo dd if=/dev/zero of=/dev/sda bs=1M count=10
-sudo dd if=/dev/zero of=/dev/sdb bs=1M count=10
+#sudo dd if=/dev/zero of=/dev/sda bs=1M count=10
+#sudo dd if=/dev/zero of=/dev/sdb bs=1M count=10
 sudo partprobe
 lsblk
